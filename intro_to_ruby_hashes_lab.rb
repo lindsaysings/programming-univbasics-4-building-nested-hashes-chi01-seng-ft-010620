04efa3616381dd0ef1106cd93faa88f4877f3775
Lindsay M <lindsaymecher@gmail.com>
2019-12-22 01:51:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,10 +22,7 @@ def monopoly_with_third_tier
   	      :key_one => 1,
   	      :key_two => 2,
   	      :key_three => 3,
-  	      :key_four => 4,
-  	      :reading_railroad => {
-  	        
-  	      }
+  	      :key_four => 4
   	    },
   	    :pieces => 4,
   	    :rent_in_dollars => {
