eba7e8dff98a05b73e81973b6d23304e445e9755
Lindsay M <lindsaymecher@gmail.com>
2019-12-22 02:02:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,7 +55,7 @@ def monopoly_with_fourth_tier
   	        
   	      },
   	      :pennsylvania_railroad => {
-  	        "mortgage_value" => "$100"
+  	        "mortgage_value" => "$200"
   	        
   	      },
   	      :reading_railroad => {
